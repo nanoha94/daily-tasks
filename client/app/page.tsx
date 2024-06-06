85ff8d514dd1b7ac2c85d521b5da420bb60ec2bf
nanoha <nanoha.creator@gmail.com>
2024-06-06 17:02:41 +0900

    開発用にトップページにログインボタンを設置（後で移動させる）

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+import { supabase } from "@/utils/supabase";
+
 const Page = () => {
-  return <div>Enter</div>;
+  const logout = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        throw error;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    // TODO: 後で削除（開発用に設置）
+    <button onClick={logout}>ログアウト</button>
+  );
 };
 
 export default Page;
