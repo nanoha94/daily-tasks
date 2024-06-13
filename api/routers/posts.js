ed239f3c67371ed4064e1dc4d27220fe28797bb7
nanoha <nanoha.creator@gmail.com>
2024-06-13 16:02:44 +0900

    投稿取得API作成

--- api/routers/posts.js
+++ api/routers/posts.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-router.post("/post", async (req, res) => {
+router.post("/", async (req, res) => {
   const { comment, tasks, category, numOfGood, authorId } = req.body;
 
   const data_tasks = tasks.map((task) => {
@@ -31,4 +31,31 @@ router.post("/post", async (req, res) => {
     res.status(500).json({ message: "サーバーエラーです" });
   }
 });
+
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await prisma.post.findMany({
+      include: { tasks: true },
+    });
+    res.status(201).json(allPosts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "サーバーエラーです" });
+  }
+});
+
+router.get("/:postId", async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: { tasks: true },
+    });
+    res.status(201).json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "サーバーエラーです" });
+  }
+});
+
 module.exports = router;
