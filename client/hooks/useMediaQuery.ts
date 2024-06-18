657fb4303f31d95d5d2db67fc9ec34ffa2e27cb4
nanoha <nanoha.creator@gmail.com>
2024-06-19 08:14:08 +0900

    fix compile error

--- client/hooks/useMediaQuery.ts
+++ client/hooks/useMediaQuery.ts
@@ -1,4 +1,5 @@
 "use client";
+import { useEventCallback } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export const mediaQuery = {
@@ -7,11 +8,21 @@ export const mediaQuery = {
 
 export const useMediaQuery = (query: string) => {
   const formattedQuery = `(${query})`;
-  const [match, setMatch] = useState<boolean>(
-    matchMedia(formattedQuery).matches
-  );
+  const [match, setMatch] = useState<boolean>(false);
 
   useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    setMatch(() => matchMedia(formattedQuery).matches);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
     const mql = matchMedia(formattedQuery);
 
     if (mql.media === "not all" || mql.media === "invalid") {
