111da5af8cdd1cefe2c31ebb62dead1f141191ad
nanoha <nanoha.creator@gmail.com>
2024-06-21 16:52:03 +0900

    Implement filter post

--- client/components/PostList.tsx
+++ client/components/PostList.tsx
@@ -1,6 +1,7 @@
 import { usePosts } from "@/contexts/PostsProvider";
 import PostItem from "./PostItem";
 import { useSearchParams } from "next/navigation";
+import { POST_CATEGORIES } from "@/costants/posts";
 
 interface Props {
   userId?: string;
@@ -11,28 +12,52 @@ const PostList = ({ userId }: Props) => {
   const searchTextParam = searchParams.get("search");
   const searchCategoryParam = searchParams.get("category");
   const { allPosts } = usePosts();
-  // const filterdPosts = allPosts.map((post) => {
-  //   if(!!searchCategoryParam )
-  // })
 
-  return (
-    <ul className="flex flex-col gap-y-2">
-      {!!allPosts &&
-        allPosts.map((post) =>
-          !!userId ? (
-            userId === post.author.id && (
+  const filterdPosts = allPosts.filter((post) => {
+    if (!!searchTextParam) {
+      if (
+        !(
+          post.comment?.includes(searchTextParam) ||
+          post.tasks.some((task) => task.content.includes(searchTextParam))
+        )
+      ) {
+        return false;
+      }
+    }
+    if (!!searchCategoryParam) {
+      const categoryId = POST_CATEGORIES.find(
+        (category) => category.key === searchCategoryParam
+      )?.id;
+      if (post.category !== categoryId) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  if (filterdPosts.length > 0) {
+    return (
+      <ul className="flex flex-col gap-y-2">
+        {filterdPosts.map(
+          (post) =>
+            post &&
+            (!!userId ? (
+              userId === post.author.id && (
+                <li key={post.id}>
+                  <PostItem post={post} />
+                </li>
+              )
+            ) : (
               <li key={post.id}>
                 <PostItem post={post} />
               </li>
-            )
-          ) : (
-            <li key={post.id}>
-              <PostItem post={post} />
-            </li>
-          )
+            ))
         )}
-    </ul>
-  );
+      </ul>
+    );
+  } else {
+    return <p>該当する投稿は見つかりません</p>;
+  }
 };
 
 export default PostList;
