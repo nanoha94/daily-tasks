29ba75593ef2468f7054c52fa49cfe19ecf620b9
nanoha <nanoha.creator@gmail.com>
2024-06-25 13:07:37 +0900

    update port for api

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const postsRoute = require("./routers/posts");
 const usersRoute = require("./routers/users");
 var cors = require("cors");
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
